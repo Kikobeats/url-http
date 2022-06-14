b8ed34106273756c86632bec53722562d946955c
Kiko Beats <josefrancisco.verdu@gmail.com>
2022-06-14 15:45:47 +0200

    refactor: typecheck over globalThis

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const URLConstructor = typeof URL === 'function' ? URL : require('url').URL
+const URL = globalThis ? globalThis.URL : require('url').URL
 
 const urlRegex = require('url-regex-safe')({
   apostrophes: true,
@@ -12,7 +12,7 @@ const REGEX_HTTP_PROTOCOL = /^https?:\/\//i
 
 module.exports = url => {
   try {
-    const { href } = new URLConstructor(url)
+    const { href } = new URL(url)
     return REGEX_HTTP_PROTOCOL.test(href) && urlRegex.test(href) && href
   } catch (err) {
     return false
--- lightweight.js
+++ lightweight.js
@@ -1,12 +1,12 @@
 'use strict'
 
-const URLConstructor = typeof URL === 'function' ? URL : require('url').URL
+const URL = globalThis ? globalThis.URL : require('url').URL
 
 const REGEX_HTTP_PROTOCOL = /^https?:\/\//i
 
 module.exports = url => {
   try {
-    const { href } = new URLConstructor(url)
+    const { href } = new URL(url)
     return REGEX_HTTP_PROTOCOL.test(href) && href
   } catch (err) {
     return false
