b0bde42b10ec32e68a6d8ce8ad54aa4507d246bf
Kiko Beats <josefrancisco.verdu@gmail.com>
2024-02-14 21:08:41 +0100

    feat: handle punycode URLs (#13)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const punycodeRegex = require('punycode-regex')()
 const urlRegex = require('url-regex-safe')
 
 const REGEX_HTTP_PROTOCOL = /^https?:\/\//i
@@ -7,12 +8,13 @@ const REGEX_HTTP_PROTOCOL = /^https?:\/\//i
 module.exports = url => {
   try {
     const { href, hostname } = new URL(url)
+    if (!REGEX_HTTP_PROTOCOL.test(href)) return false
     const isIPv6 = hostname.startsWith('[') && hostname.endsWith(']')
+    const isPunycode = punycodeRegex.test(hostname)
+    const exact = !isIPv6 && !isPunycode
+    const tlds = isPunycode ? [] : undefined
     return (
-      REGEX_HTTP_PROTOCOL.test(href) &&
-      urlRegex({ apostrophes: true, exact: !isIPv6, parens: true }).test(
-        href
-      ) &&
+      urlRegex({ apostrophes: true, exact, parens: true, tlds }).test(href) &&
       href
     )
   } catch (_) {
